7d12c8061b1371d7f5ce34a3f14d5848f0b48180
IT24102296 <it24102296@my.sliit.lk>
2024-08-27 21:34:32 -0700

    IT24102296 Diwanjani B.S.S

---
+++ Q1/IT24102296Lab6Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT24102296Lab6Q1
+{
+  public static void main(String[] args)
+  {
+  Scanner input=new Scanner(System.in);
+  double number;
+  System.out.println("Enter a number :");
+  number=input.nextDouble();
+
+  double square=(number*number);
+  double squareRoot=Math.sqrt(number);
+
+  System.out.println("The square of "+number+" is :"+square);
+  System.out.println("The square root of "+number+" is :"+squareRoot);
+  }
+} 
