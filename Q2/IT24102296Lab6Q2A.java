d52b5f988efa8baacdc457e95c8b64539b504b1a
IT24102296 <it24102296@my.sliit.lk>
2024-08-27 21:36:00 -0700

    IT24102296 Diwanjani B.S.S

---
+++ Q2/IT24102296Lab6Q2A.java
@@ -0,0 +1,15 @@
+public class IT24102296Lab6Q2A
+{
+  public static void main(String[] args)
+  {
+  
+    int number=1;
+    while(number<=10)
+    {
+    System.out.print(number+" ");
+    number=number+1;
+    }
+  }
+}
+  
+  
