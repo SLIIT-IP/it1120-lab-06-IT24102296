0f5420f42517182c52718c3fbcfa0872d01b8aab
IT24102296 <it24102296@my.sliit.lk>
2024-09-04 11:16:05 +0530

    IT24102296 Diwanjani B.S.S

---
+++ IT24102296Lab6Q2B.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class IT24102296Lab6Q2B
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Please enter 10 numbers:");
+    Scanner input=new Scanner(System.in);
+    int[] numbers=new int[10];
+    int count=0;
+
+    while(count<10)
+    {
+	System.out.print("Enter number "+(count+1) +":");
+	numbers[count]=input.nextInt();
+ 	count++;
+    }
+    System.out.println();
+
+    System.out.println("The number you entered are: ");
+    count=0;
+    while(count<10)
+    {
+	System.out.print(numbers[count]+" ");
+	count++;
+    }
+  }
+}
